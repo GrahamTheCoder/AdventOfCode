9c95e5932c496da51ce0b9ad030fcfb8a28e7229
GrahamTheCoder <GrahamTheCoder@gmail.com>
2022-12-06 00:12:13 +0000

    Day 2 Part 2
    Overly complicated modulus just for practice

--- 2022/02/main.hs
+++ 2022/02/main.hs
@@ -7,15 +7,22 @@ import Text.Read
 decrypt :: [Char] -> (Int, Int)
 decrypt [o, ' ', m] = (1 + (ord o) - (ord 'A'), 1 + (ord m) - (ord 'X'))
 
-getScore :: (Int, Int) -> Int
-getScore (o, m)
+getScoreFromOpponentAndMine :: (Int, Int) -> Int
+getScoreFromOpponentAndMine (o, m)
     | o == m = 3 + m
     | m - 1 == o || m + 2 == o = 6 + m
     | otherwise = 0 + m
 
+getScoreFromOpponentAndStrat :: (Int, Int) -> Int
+getScoreFromOpponentAndStrat (o, 1) = 1 + mod (o - 2) 3
+getScoreFromOpponentAndStrat (o, 2) = 3 + o
+getScoreFromOpponentAndStrat (o, 3) = 6 + 1 + mod o 3
+
 main :: IO Int
 main = do
     contents <- readFile "input.txt"
-    let inputLines = lines contents
-    let score = fmap (getScore. decrypt) inputLines
-    return $ sum score
+    let inputLines = fmap decrypt $ lines contents
+    let part1Score = sum $ fmap getScoreFromOpponentAndMine inputLines
+    let part2Score = sum $ fmap getScoreFromOpponentAndStrat inputLines
+    putStrLn $ show (part1Score, part2Score)
+    return part2Score
