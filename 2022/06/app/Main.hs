a8de9966bc5c7b23b1159ddeb73dd6b5cd313505
GrahamTheCoder <GrahamTheCoder@gmail.com>
2022-12-11 12:05:20 +0000

    Day 6 Part 2
    3263

--- 2022/06/app/Main.hs
+++ 2022/06/app/Main.hs
@@ -6,5 +6,6 @@ main :: IO ()
 main = do
     contents <- readFile "input.txt"
     putStrLn $ show $ endOfFirstUniqueRun 4 contents
+    putStrLn $ show $ endOfFirstUniqueRun 14 contents
     return ()
 
