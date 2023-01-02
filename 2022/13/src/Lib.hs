b5bc635b697562516de73a636b1fc4e7ab7d133a
GrahamTheCoder <GrahamTheCoder@gmail.com>
2023-01-02 13:53:08 +0000

    Parse into tuples

--- 2022/13/src/Lib.hs
+++ 2022/13/src/Lib.hs
@@ -15,6 +15,23 @@ import qualified Text.Megaparsec.Char as M
 import qualified Text.Megaparsec.Char.Lexer as L
 import Debug.Trace (traceShow, traceShowId)
 
+type MParser = Parsec Void String
+data Node a = Leaf a | Branches [Node a] deriving (Show)
+
+parseNode :: Num a => MParser (Node a)
+parseNode = Leaf <$> L.decimal <|> 
+    Branches <$> M.between (M.char '[') (M.char ']') (parseNode `M.sepBy` M.char ',')
+
+parsePacketPair :: MParser (Node Int, Node Int)
+parsePacketPair = do
+    n1 <- parseNode
+    M.newline
+    n2 <- parseNode
+    return (n1, n2)
+
+parseFile fileContents = case M.runParser (parsePacketPair `M.sepBy1` some M.newline) "" fileContents of
+    Left s -> error (show s)
+    Right m -> return m
 
 areCorrectOrder :: String -> [Bool]
-areCorrectOrder _ = []
+areCorrectOrder str = map (const True) (traceShowId $ head (parseFile str))
