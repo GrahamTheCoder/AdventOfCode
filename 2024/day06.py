97ebd40f46309d72881254bd2ad93962d8331708
Graham <grahamthecoder@gmail.com>
2024-12-12 01:14:37 +0000

    Manual tidy up to dedupe things

--- 2024/day06.py
+++ 2024/day06.py
@@ -1,126 +1,122 @@
 class MapWithObstacle:
-    def __init__(self, map_grid, obstacle_position=None):
-        self.map_grid = map_grid
-        self.obstacle_position = obstacle_position
+    _turn_right = {"^": ">", ">": "v", "v": "<", "<": "^"}
+    _direction_vectors = {"^": (0, -1), ">": (1, 0), "v": (0, 1), "<": (-1, 0)}
 
-    def is_obstacle(self, x, y):
-        if self.obstacle_position and (x, y) == self.obstacle_position:
+    def __init__(self, map_grid):
+        self._map_grid = map_grid
+        self._extra_obstacle_position = (None, None)
+        self._visited_position_directions = set()
+        self._next_position = {}
+
+    # Not thread safe due to state mutation
+    def get_visited_positions(self, guard_position, guard_direction):
+        self.guard_route_is_loop(guard_position, guard_direction)
+        return {(x, y) for (x, y, _) in self._visited_position_directions}
+
+    # Not thread safe due to state mutation
+    def is_loop_with_obstacle(self, guard_position, guard_direction, obstacle_position):
+        self._extra_obstacle_position = obstacle_position
+        self._visited_position_directions = set()
+        is_loop = self.guard_route_is_loop(guard_position, guard_direction)
+        self._extra_obstacle_position = (None, None)
+        return is_loop
+
+    def guard_route_is_loop(self, guard_position, guard_direction):
+
+        x, y = guard_position
+        direction = guard_direction
+        positions_since_turn = []
+        while True:
+            x, y = self._jump(x, y, direction)
+
+            if (x, y, direction) in self._visited_position_directions:
+                return True
+            if not self._in_line_with_extra_obstacle(x, y, direction):
+                positions_since_turn.append((x, y))
+            self._visited_position_directions.add((x, y, direction))
+
+            new_x, new_y = self._move_forward(x, y, direction)
+
+            if not self._is_on_grid(new_x, new_y):
+                return False
+
+            if self._is_obstacle(new_x, new_y):
+                for prev_x, prev_y in positions_since_turn:
+                    self._next_position[prev_x, prev_y, direction] = (x, y)
+                positions_since_turn = []
+                direction = MapWithObstacle._turn_right[direction]
+            else:
+                x, y = new_x, new_y
+
+    def _move_forward(self, x, y, direction):
+        dx, dy = MapWithObstacle._direction_vectors[direction]
+        return x + dx, y + dy
+
+    def _is_on_grid(self, x, y):
+        return 0 <= x < len(self._map_grid[0]) and 0 <= y < len(self._map_grid)
+
+    def _in_line_with_extra_obstacle(self, x, y, direction):
+        (obs_x, obs_y) = self._extra_obstacle_position
+        return x == obs_x and direction in "^v" or y == obs_y and direction in "<>"
+
+    def _jump(self, x, y, direction):
+        if self._in_line_with_extra_obstacle(x, y, direction):
+            return (x, y)
+        return self._next_position.get((x, y, direction), (x, y))
+
+    def _is_extra_obstacle(self, x, y):
+        if (x, y) == self._extra_obstacle_position:
             return True
-        return self.map_grid[y][x] != '.'
+
+    def _is_obstacle(self, x, y):
+        return self._is_extra_obstacle(x, y) or self._map_grid[y][x] != "."
+
 
 def parse_input(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r", encoding="utf8") as file:
         lines = file.readlines()
-    
+
     map_grid = []
     guard_position = None
     guard_direction = None
-    
+
     for y, line in enumerate(lines):
         row = list(line.strip())
         map_grid.append(row)
         for x, char in enumerate(row):
-            if char in '^>v<':
+            if char in "^>v<":
                 guard_position = (x, y)
                 guard_direction = char
-                row[x] = '.'
-    
+                row[x] = "."
+
     return map_grid, guard_position, guard_direction
 
-def move_guard(map_with_obstacle, guard_position, guard_direction):
-    direction_vectors = {
-        '^': (0, -1),
-        '>': (1, 0),
-        'v': (0, 1),
-        '<': (-1, 0)
-    }
-    
-    turn_right = {
-        '^': '>',
-        '>': 'v',
-        'v': '<',
-        '<': '^'
-    }
-    
-    visited_positions = set()
-    x, y = guard_position
-    visited_positions.add((x, y))
-    
-    while True:
-        dx, dy = direction_vectors[guard_direction]
-        new_x, new_y = x + dx, y + dy
-        
-        if 0 <= new_x < len(map_with_obstacle.map_grid[0]) and 0 <= new_y < len(map_with_obstacle.map_grid):
-            if not map_with_obstacle.is_obstacle(new_x, new_y):
-                x, y = new_x, new_y
-                visited_positions.add((x, y))
-            else:
-                guard_direction = turn_right[guard_direction]
-        else:
-            break
-    
-    return visited_positions
 
-def detect_loops(map_with_obstacle, guard_position, guard_direction):
-    direction_vectors = {
-        '^': (0, -1),
-        '>': (1, 0),
-        'v': (0, 1),
-        '<': (-1, 0)
+def find_obstacle_positions_causing_loops(
+    map_with_obstacle, visited_positions, guard_position, guard_direction
+):
+    # PERF optimisation available: start guard right before the obstacle
+    return {
+        pos
+        for pos in visited_positions
+        if map_with_obstacle.is_loop_with_obstacle(guard_position, guard_direction, pos)
     }
-    
-    turn_right = {
-        '^': '>',
-        '>': 'v',
-        'v': '<',
-        '<': '^'
-    }
-    
-    loop_positions = set()
-    visited_positions = set()
-    x, y = guard_position
-    direction = guard_direction
-    
-    while True:
-        if (x, y, direction) in visited_positions:
-            loop_positions.add((x, y))
-            break
-        visited_positions.add((x, y, direction))
-        
-        dx, dy = direction_vectors[direction]
-        new_x, new_y = x + dx, y + dy
-        
-        if 0 <= new_x < len(map_with_obstacle.map_grid[0]) and 0 <= new_y < len(map_with_obstacle.map_grid):
-            if not map_with_obstacle.is_obstacle(new_x, new_y):
-                x, y = new_x, new_y
-            else:
-                direction = turn_right[direction]
-        else:
-            break
-    
-    return loop_positions
 
-def find_obstacle_positions_causing_loops(map_grid, guard_position, guard_direction):
-    map_with_obstacle = MapWithObstacle(map_grid)
-    visited_positions = move_guard(map_with_obstacle, guard_position, guard_direction)
-    loop_causing_positions = set()
-    
-    for pos in visited_positions:
-        map_with_obstacle.obstacle_position = pos
-        if detect_loops(map_with_obstacle, guard_position, guard_direction):
-            loop_causing_positions.add(pos)
-    
-    return loop_causing_positions
 
 def count_distinct_positions(file_path):
     map_grid, guard_position, guard_direction = parse_input(file_path)
     map_with_obstacle = MapWithObstacle(map_grid)
-    visited_positions = move_guard(map_with_obstacle, guard_position, guard_direction)
-    loop_causing_positions = find_obstacle_positions_causing_loops(map_grid, guard_position, guard_direction)
+
+    visited_positions = map_with_obstacle.get_visited_positions(
+        guard_position, guard_direction
+    )
+    loop_causing_positions = find_obstacle_positions_causing_loops(
+        map_with_obstacle, visited_positions, guard_position, guard_direction
+    )
     return len(visited_positions), len(loop_causing_positions)
 
+
 if __name__ == "__main__":
-    file_path = 'inputs/06.txt'
-    distinct_positions, loop_positions = count_distinct_positions(file_path)
+    distinct_positions, loop_positions = count_distinct_positions("inputs/06.txt")
     print(f"Distinct positions visited: {distinct_positions}")
     print(f"Positions causing loops: {loop_positions}")
