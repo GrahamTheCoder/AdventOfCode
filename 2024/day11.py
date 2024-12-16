ab82a04ce6fe0f56e1ba03e9f7d99961c0da337a
Graham <grahamthecoder@gmail.com>
2024-12-16 21:26:38 +0000

    Performance prompt:
    Hasn't terminated after a few minutes...we'll need some performance optimisations. Think about ways to avoid duplicate work then apply the most impactful improvement only

--- 2024/day11.py
+++ 2024/day11.py
@@ -3,24 +3,30 @@ def parse_input(file_path):
         stones = list(map(int, file.readline().strip().split()))
     return stones
 
-def transform_stones(stones):
+def transform_stones(stones, memo):
     new_stones = []
     for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif len(str(stone)) % 2 == 0:
-            half_len = len(str(stone)) // 2
-            left_half = int(str(stone)[:half_len])
-            right_half = int(str(stone)[half_len:])
-            new_stones.extend([left_half, right_half])
+        if stone in memo:
+            new_stones.extend(memo[stone])
         else:
-            new_stones.append(stone * 2024)
+            if stone == 0:
+                transformed = [1]
+            elif len(str(stone)) % 2 == 0:
+                half_len = len(str(stone)) // 2
+                left_half = int(str(stone)[:half_len])
+                right_half = int(str(stone)[half_len:])
+                transformed = [left_half, right_half]
+            else:
+                transformed = [stone * 2024]
+            memo[stone] = transformed
+            new_stones.extend(transformed)
     return new_stones
 
 def main():
     stones = parse_input('inputs/11.txt')
+    memo = {}
     for _ in range(75):
-        stones = transform_stones(stones)
+        stones = transform_stones(stones, memo)
     print(len(stones))
 
 if __name__ == "__main__":
