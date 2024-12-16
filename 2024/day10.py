d1923c12e30c610117101e2c07c5b7529d90a9c1
Graham <grahamthecoder@gmail.com>
2024-12-16 21:18:36 +0000

    day 10 part 2 (unedited from 4o)

--- 2024/day10.py
+++ 2024/day10.py
@@ -11,35 +11,34 @@ def find_trailheads(map_data):
                 trailheads.append((i, j))
     return trailheads
 
-def bfs(map_data, start):
-    from collections import deque
+def dfs(map_data, x, y, visited):
+    if (x, y) in visited:
+        return 0
+    if map_data[x][y] == 9:
+        return 1
     
+    visited.add((x, y))
+    count = 0
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    queue = deque([start])
-    visited = set()
-    visited.add(start)
-    score = 0
     
-    while queue:
-        x, y = queue.popleft()
-        if map_data[x][y] == 9:
-            score += 1
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < len(map_data) and 0 <= ny < len(map_data[0]) and (nx, ny) not in visited:
-                if map_data[nx][ny] == map_data[x][y] + 1:
-                    queue.append((nx, ny))
-                    visited.add((nx, ny))
-    return score
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < len(map_data) and 0 <= ny < len(map_data[0]):
+            if map_data[nx][ny] == map_data[x][y] + 1:
+                count += dfs(map_data, nx, ny, visited)
+    
+    visited.remove((x, y))
+    return count
 
-def calculate_total_score(map_data):
+def calculate_total_rating(map_data):
     trailheads = find_trailheads(map_data)
-    total_score = 0
+    total_rating = 0
     for trailhead in trailheads:
-        total_score += bfs(map_data, trailhead)
-    return total_score
+        visited = set()
+        total_rating += dfs(map_data, trailhead[0], trailhead[1], visited)
+    return total_rating
 
 if __name__ == "__main__":
     map_data = parse_topographic_map('inputs/10.txt')
-    total_score = calculate_total_score(map_data)
-    print(total_score)
+    total_rating = calculate_total_rating(map_data)
+    print(total_rating)
