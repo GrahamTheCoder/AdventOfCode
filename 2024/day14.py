dee56bacf295a036c9041a3fcf37d6f951810a4c
GrahamTheCoder <GrahamTheCoder@gmail.com>
2025-01-22 01:40:32 +0000

    R1-32B: day 14 part 2 fail - I hinted the tree might be symmetric. Solution doesn't terminate

--- 2024/day14.py
+++ 2024/day14.py
@@ -12,43 +12,34 @@ def parse_input():
             robots.append((x, y, vx, vy))
     return robots
 
-def compute_quadrant_counts(width, height, robots):
-    mid_x = (width - 1) // 2
-    mid_y = (height - 1) // 2
-    quad_i = 0
-    quad_ii = 0
-    quad_iii = 0
-    quad_iv = 0
+def is_symmetric(robots, width, height, t):
+    positions = []
+    for (x, y, vx, vy) in robots:
+        new_x = (x + vx * t) % width
+        new_y = (y + vy * t) % height
+        positions.append((new_x, new_y))
+    
+    position_set = set(positions)
+    
+    for (x, y) in positions:
+        mirrored_x = width - x - 1
+        if (mirrored_x, y) not in position_set:
+            return False
+    return True
 
-    for x, y, vx, vy in robots:
-        new_x = (x + vx * 100) % width
-        new_y = (y + vy * 100) % height
-
-        if new_x == mid_x or new_y == mid_y:
-            continue
-
-        if new_x < mid_x:
-            if new_y < mid_y:
-                quad_i += 1
-            else:
-                quad_iii += 1
-        else:
-            if new_y < mid_y:
-                quad_ii += 1
-            else:
-                quad_iv += 1
-
-    return (quad_i, quad_ii, quad_iii, quad_iv)
+def find_min_time(robots, width, height):
+    t = 0
+    while True:
+        if is_symmetric(robots, width, height, t):
+            return t
+        t += 1
 
 def main():
     width = 101
     height = 103
     robots = parse_input()
-    counts = compute_quadrant_counts(width, height, robots)
-    safety_factor = 1
-    for cnt in counts:
-        safety_factor *= cnt
-    print(safety_factor)
+    min_time = find_min_time(robots, width, height)
+    print(f"The fewest number of seconds required is: {min_time}")
 
 if __name__ == "__main__":
     main()
